d7550f598ec49234572ac1d5ba88a148162aac64
Mikhail Morozov <inferst@gmail.com>
2024-04-20 22:19:06 +0300

    fix: export class

---
+++ src/main.ts
@@ -0,0 +1,75 @@
+import { Dudes } from "./dudes";
+import { SettingsEntity } from "./types";
+import "./styles.css";
+
+const manifest = {
+  bundles: [
+    {
+      name: "main",
+      assets: [
+        {
+          alias: "dude",
+          src: "/sprites/dude/dude.json",
+        },
+        {
+          alias: "sith",
+          src: "/sprites/sith/sith.json",
+        },
+        {
+          alias: "agent",
+          src: "/sprites/agent/agent.json",
+        },
+        {
+          alias: "girl",
+          src: "/sprites/girl/girl.json",
+        },
+        {
+          alias: "senior",
+          src: "/sprites/senior/senior.json",
+        },
+        {
+          alias: "cat",
+          src: "/sprites/cat/cat.json",
+        },
+        {
+          alias: "nerd",
+          src: "/sprites/nerd/nerd.json",
+        },
+      ],
+    },
+    {
+      name: "fonts",
+      assets: [
+        {
+          alias: "Rubik",
+          src: "/fonts/Rubik.ttf",
+        },
+      ],
+    },
+  ],
+};
+
+const sound = { jump: "/sounds/jump.mp3" };
+const settings: SettingsEntity = {
+  fallingDudes: true,
+};
+
+const root = document.getElementById("root");
+
+if (root) {
+  const dudes = new Dudes(root);
+  await dudes.run({ manifest, sound });
+  dudes.updateSettings(settings);
+
+  setTimeout(() => {
+    dudes.processMessage({
+      message: "Hello!",
+      userId: "1",
+      emotes: [],
+      info: {
+        color: "pink",
+        displayName: "Dude",
+      },
+    });
+  }, 500);
+}
